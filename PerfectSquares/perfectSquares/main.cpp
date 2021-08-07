43c20408e127372416d5951fa21b6018d4adbcdf
Jabe <jabe_wang@foxmail.com>
2021-08-07 08:44:27 -0400

    The second question for perfect squares.

---
+++ PerfectSquares/perfectSquares/main.cpp
@@ -0,0 +1,33 @@
+#include <QCoreApplication>
+#include <iostream>
+
+/***************************************
+Given an integer n, return the least number
+of perfect square numbers that sum to n.
+
+A perfect square is an integer that is the
+square of an integer; in other words, it is
+the product of some integer with itself. For
+example, 1, 4, 9, and 16 are perfect squares
+while 3 and 11 are not.
+
+Input: n = 12
+Output: 3
+Explanation: 12 = 4 + 4 + 4.
+
+
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+****************************************/
+
+
+
+int main()
+{
+//    QCoreApplication a(argc, argv);
+//    return a.exec();
+
+
+    return 0;
+}
